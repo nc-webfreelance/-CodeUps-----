2caeddf1fac6bb3067cf87a2fc918d9366a65396
Hina Akamatsu <moment.20244@icloud.com>
2024-02-25 15:32:40 +0900

    ３回目フィードバック後修正②

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,43 @@
 jQuery(function ($) {
-  // この中であればWordpressでも「$」が使用可能になる
-  var topBtn = $(".to-top");
-  topBtn.hide();
 
-  // ボタンの表示設定
+
+
+// ヘッダー
+  let header = $(".js-header");
+  let headerHeight = $(".js-header").height();
+  let height = $(".js-mv-height").height();
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > height - headerHeight) {
+      header.addClass("is-color");
+    } else {
+      header.removeClass("is-color");
+    }
+  });
+  // ヘッダーの高さ分だけコンテンツを下げる
+  $("main").css("margin-top", headerHeight);
+  // ヘッダーの高さ取得
+  $('a[href^="#"]').click(function () {
+    const speed = 1000;
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? "html" : href);
+    // ヘッダーの高さ分下げる
+    let position = target.offset().top - headerHeight;
+    $("body,html").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
+
+
+// トップへ戻るボタン
+  let topBtn = $(".to-top");
+  topBtn.hide();
   $(window).scroll(function () {
     if ($(this).scrollTop() > 70) {
-      // 指定px以上のスクロールでボタンを表示
       topBtn.fadeIn();
     } else {
-      // 画面が指定pxより上ならボタンを非表示
       topBtn.fadeOut();
     }
   });
-
-  // ボタンをクリックしたらスクロールして上に戻る
   topBtn.click(function () {
     $("body,html").animate(
       {
@@ -27,63 +50,7 @@ jQuery(function ($) {
   });
 
 
-
-  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
-
-  $(document).on("click", 'a[href*="#"]', function () {
-    let time = 400;
-    let header = $("header").innerHeight();
-    let target = $(this.hash);
-    if (!target.length) return;
-    let targetY = target.offset().top - header;
-    $("html,body").animate({ scrollTop: targetY }, time, "swing");
-    return false;
-  });
-
-  $(".js-hamburger,.js-sp-nav,.js-drawer a").click(function () {
-    if ($(".js-hamburger").hasClass("is-active")) {
-      $(".js-hamburger").removeClass("is-active");
-      $("body").removeClass("no-scroll");
-      $(".header").removeClass("is-active");
-      $(".js-sp-nav").fadeOut(500);
-      // $(".mv__header").fadeIn(500);
-    } else {
-      $(".js-hamburger").addClass("is-active");
-      $("body").addClass("no-scroll");
-      $(".header").addClass("is-active");
-      $(".js-sp-nav").fadeIn(500);
-      // $(".mv__header").fadeOut(500);
-    }
-  });
-
-
-  // ヘッダークラス名付与
-  let header = $(".js-header");
-  let headerHeight = $(".js-header").height();
-  let height = $(".js-mv-height").height();
-
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > height - headerHeight) {
-      header.addClass("is-color");
-    } else {
-      header.removeClass("is-color");
-    }
-  });
-
-  // ヘッダーの高さ分だけコンテンツを下げる
-  $("main").css("margin-top", headerHeight);
-
-  // ヘッダーの高さ取得
-  $('a[href^="#"]').click(function () {
-    const speed = 1000;
-    let href = $(this).attr("href");
-    let target = $(href == "#" || href == "" ? "html" : href);
-    // ヘッダーの高さ分下げる
-    let position = target.offset().top - headerHeight;
-    $("body,html").animate({ scrollTop: position }, speed, "swing");
-    return false;
-  });
-
+// スワイパー
   const swiper = new Swiper(".js-swiper", {
     loop: true,
     effect: "fade",
@@ -93,10 +60,7 @@ jQuery(function ($) {
       delay: 3000,
     },
   });
-  
   const campaignSwiper = new Swiper(".js-campaign-swiper", {
-    // centeredSlides: true,
-    // KeyboardEvent:true,
     loop: true,
     speed: 1500,
     autoplay: {
@@ -107,7 +71,7 @@ jQuery(function ($) {
     spaceBetween: 24,
     breakpoints: {
       768: {
-    spaceBetween: 40,
+        spaceBetween: 40,
       },
     },
     navigation: {
@@ -117,37 +81,9 @@ jQuery(function ($) {
   });
 
 
-  // var swiper = new Swiper(".js-campaign-swiper", {
-  //   loop: true,
-  //   speed: 1500,
-  //   autoplay: {
-  //     delay: 1500,
-  //     disableOnInteraction: false,
-  //   },
-  //   // Optional parameters
-  //   slidesPerView: 'auto',
-  //   spaceBetween: 24,
-
-  //   breakpoints: {
-  //     // when window width is >= 768px
-  //     768: {
-  //       spaceBetween: 40
-  //     },
-  //   },
-
-  //   // Navigation arrows
-  //   navigation: {
-  //     prevEl: '.swiper-button-prev',
-  //     nextEl: '.swiper-button-next',
-  //   }
-  // });
-
-
-  //要素の取得とスピードの設定
-  var box = $(".information__image,.wide-card__image,.price__image"),
+// 画像を出現するアニメーション
+  let box = $(".information__image,.wide-card__image,.price__image"),
     speed = 700;
-
-  //.colorboxの付いた全ての要素に対して下記の処理を行う
   box.each(function () {
     $(this).append('<div class="img-color"></div>');
     var color = $(this).find($(".img-color")),
@@ -155,7 +91,6 @@ jQuery(function ($) {
     var counter = 0;
     image.css("opacity", "0");
     color.css("width", "0%");
-    //inviewを使って背景色が画面に現れたら処理をする
     color.on("inview", function () {
       if (counter == 0) {
         $(this)
@@ -171,38 +106,54 @@ jQuery(function ($) {
   });
 
 
-
-$(function () {
-  $(".js-faq-question").addClass("is-open");
-  $(".js-faq-question").on("click", function () {
-    $(this).toggleClass("is-open");
-    $(this).next().slideToggle(300);
+// ハンバーガー,ドロワー
+  $(".js-hamburger,.js-sp-nav,.js-drawer a").click(function () {
+    if ($(".js-hamburger").hasClass("is-active")) {
+      $(".js-hamburger").removeClass("is-active");
+      $("body").removeClass("no-scroll");
+      $(".header").removeClass("is-active");
+      $(".js-sp-nav").fadeOut(500);
+    } else {
+      $(".js-hamburger").addClass("is-active");
+      $("body").addClass("no-scroll");
+      $(".header").addClass("is-active");
+      $(".js-sp-nav").fadeIn(500);
+    }
   });
-});
 
 
-$(function(){
-  $(".gallery-list__item img").click(function() {
-    $("#gallery__modal-image").html($(this).prop('outerHTML'));
-    $("#gallery__modal-image").fadeIn(100);
-    $("body").addClass("no-scroll");
+// FAQ
+  $(function () {
+    $(".js-faq-question").addClass("is-open");
+    $(".js-faq-question").on("click", function () {
+      $(this).toggleClass("is-open");
+      $(this).next().slideToggle(300);
+    });
   });
-  $("#gallery__modal-image, #gallery__modal-image img").click(function() {
-    $("#gallery__modal-image").fadeOut(100);
-    $("body").removeClass("no-scroll");
-  });
-});
 
-jQuery(function ($) {
-  $('.js-tab-menu').on('click', function () {
-      $('.js-tab-menu').removeClass('tab-active');
-      $('.js-tab-content').removeClass('tab-active');
-      $(this).addClass('tab-active');
+
+// Gallery
+  $(function () {
+    $(".gallery-list__item img").click(function () {
+      $("#gallery__modal-image").html($(this).prop("outerHTML"));
+      $("#gallery__modal-image").fadeIn(100);
+      $("body").addClass("no-scroll");
+    });
+    $("#gallery__modal-image, #gallery__modal-image img").click(function () {
+      $("#gallery__modal-image").fadeOut(100);
+      $("body").removeClass("no-scroll");
+    });
+  });
+
+
+// Informationのタブ
+  jQuery(function ($) {
+    $(".js-tab-menu").on("click", function () {
+      $(".js-tab-menu").removeClass("tab-active");
+      $(".js-tab-content").removeClass("tab-active");
+      $(this).addClass("tab-active");
       var number = $(this).data("number");
-      $('#' + number).addClass('tab-active');
+      $("#" + number).addClass("tab-active");
+    });
   });
-});
-
-
-
 });
